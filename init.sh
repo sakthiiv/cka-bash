d0ad0de727661701fd026d5de665f1fd2cc5bf4b
Sakthivel Viswanathan <visvel@gmail.com>
2022-05-21 23:13:37 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 
 alias k='kubectl'
+alias ke='kubectl edit'
 alias kd='kubectl describe'
 alias kg='kubectl get'
 alias kc='kubectl create -f'
 alias ka='kubectl apply -f'
 alias kl='kubectl logs'
+alias kr='kubectl run'
